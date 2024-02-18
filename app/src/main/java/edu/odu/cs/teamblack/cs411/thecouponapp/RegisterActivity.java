8445a28d65bb69a3201f00b316df0a4f2c7da06d
cs-cbonh002 <bonham_36@hotmail.com>
2024-02-18 18:41:14 -0500

    Error fix and update to connect buttons on register and login.

--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/RegisterActivity.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/RegisterActivity.java
@@ -2,7 +2,10 @@ package edu.odu.cs.teamblack.cs411.thecouponapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btn=findViewById(R.id.login_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
