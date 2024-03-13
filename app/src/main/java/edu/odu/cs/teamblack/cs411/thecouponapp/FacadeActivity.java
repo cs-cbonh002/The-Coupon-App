8247d975932293da2bfcc05fa927d4a0586a3e49
RedstoneRm <smcal004@odu.edu>
2024-03-12 20:31:01 -0400

    FacadeActivity Button Operation

--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/FacadeActivity.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/FacadeActivity.java
@@ -2,7 +2,11 @@ package edu.odu.cs.teamblack.cs411.thecouponapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FacadeActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class FacadeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facade);
+
+        ImageButton btn = findViewById(R.id.imageButton);
+
+        btn.setOnClickListener(v -> startActivity(new Intent(FacadeActivity.this, HomeActivity.class)));
+
+        btn = findViewById(R.id.imageButton2);
+        btn.setOnClickListener(v -> startActivity(new Intent(FacadeActivity.this, HomeActivity.class)));
+
+        btn = findViewById(R.id.imageButton3);
+        btn.setOnClickListener(v -> startActivity(new Intent(FacadeActivity.this, HomeActivity.class)));
     }
 }
