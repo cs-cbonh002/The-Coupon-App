3d4cf012f6615feea0d63236f47622df46a9c22c
cs-cbonh002 <bonham_36@hotmail.com>
2024-03-15 18:20:24 -0400

    Change Safe Exit Button to go to Facade. Made Logout functional.

--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/HomeActivity.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/HomeActivity.java
@@ -63,10 +63,13 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         if (item.getItemId() == R.id.nav_safe_exit)
             if (item.getItemId() == R.id.nav_safe_exit) {
                 // Perform logout action
-                logout();
+                safeExit();
             }
         if (item.getItemId() == R.id.nav_logout)
+        {
             Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+            logout();
+        }
 
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
@@ -79,6 +82,15 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         } else {
             super.onBackPressed();
         }
+    }
+
+    private void safeExit() {
+        // Implement your logout logic here
+        // For example, clearing user session, resetting preferences, etc.
+        // Once logged out, you may navigate back to the login screen
+        Intent intent = new Intent(HomeActivity.this, FacadeActivity.class);
+        startActivity(intent);
+        finish(); // Optional: Close the current activity
     }
 
     private void logout() {
