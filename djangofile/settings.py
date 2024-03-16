f741e93b203ec04e2f6fc056e177ce5fa8e80c18
Kenneth Sherrod <ksher009@odu.edu>
2024-03-16 17:51:58 -0400

    django file settings changes

--- djangofile/settings.py
+++ djangofile/settings.py
@@ -75,19 +75,12 @@ WSGI_APPLICATION = 'databaseproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-
-        'NAME':'',
-
-        'USER': '',
-
-        'PASSWORD': '',
-
-        'HOST': '',
-
-        'PORT': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'couponapp', 
+        'USER': 'postgres',
+        'PASSWORD': 'queens39',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
