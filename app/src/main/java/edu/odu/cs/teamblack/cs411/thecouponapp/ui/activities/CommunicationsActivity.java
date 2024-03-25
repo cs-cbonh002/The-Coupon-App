f4d3872ed36e836284aedc054a8009e95eeae89a
cs-cbonh002 <bonham_36@hotmail.com>
2024-03-24 20:34:22 -0400

    Major update. Home screen and nav bar rework to work with activities. Working, but not complete.

--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/activities/CommunicationsActivity.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/activities/CommunicationsActivity.java
@@ -1,16 +1,147 @@
 package edu.odu.cs.teamblack.cs411.thecouponapp.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import edu.odu.cs.teamblack.cs411.thecouponapp.R;
+import edu.odu.cs.teamblack.cs411.thecouponapp.utils.SharedPreferences;
 
 public class CommunicationsActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home, communications, local_resources, safe_exit, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_communications);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        communications = findViewById(R.id.communications);
+        local_resources = findViewById(R.id.local_resources);
+        safe_exit = findViewById(R.id.safe_exit);
+        logout = findViewById(R.id.logout);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrawer(drawerLayout);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(CommunicationsActivity.this, HomeActivity.class);
+            }
+        });
+        communications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recreate();
+            }
+        });
+
+        local_resources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(CommunicationsActivity.this, LocalResourcesActivity.class);
+            }
+        });
+
+        safe_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                safeExit();
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+    }
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public static void redirectActivity(Activity activity, Class secondActivity) {
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.safe_exit_btn)
+        {
+            safeExit();
+        }
+        return true;
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void safeExit() {
+        // Implement your logout logic here
+        // For example, clearing user session, resetting preferences, etc.
+        // Once logged out, you may navigate back to the login screen
+        Intent intent = new Intent(CommunicationsActivity.this, FacadeActivity.class);
+        startActivity(intent);
+        finish(); // Optional: Close the current activity
+    }
+
+    private void logout() {
+        SharedPreferences.clearAccessToken(this); // Clear the access token
+        Intent intent = new Intent(CommunicationsActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish(); // Optional: Close the current activity
     }
 }
--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/activities/HomeActivity.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/activities/HomeActivity.java
@@ -6,10 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -21,27 +26,88 @@ import edu.odu.cs.teamblack.cs411.thecouponapp.ui.fragments.ProfileSettingsFragm
 import edu.odu.cs.teamblack.cs411.thecouponapp.R;
 import edu.odu.cs.teamblack.cs411.thecouponapp.utils.SharedPreferences;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity  {
 
-    private DrawerLayout drawerLayout;
+    DrawerLayout drawerLayout;
+    ImageView menu;
+    LinearLayout home, communications, local_resources, safe_exit, logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
-        setSupportActionBar(toolbar);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav,
-                R.string.close_nav);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_home);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        menu = findViewById(R.id.menu);
+        home = findViewById(R.id.home);
+        communications = findViewById(R.id.communications);
+        local_resources = findViewById(R.id.local_resources);
+        safe_exit = findViewById(R.id.safe_exit);
+        logout = findViewById(R.id.logout);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDrawer(drawerLayout);
+            }
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recreate();
+            }
+        });
+        communications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(HomeActivity.this, CommunicationsActivity.class);
+            }
+        });
+
+        local_resources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                redirectActivity(HomeActivity.this, LocalResourcesActivity.class);
+            }
+        });
+
+        safe_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                safeExit();
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+    }
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
         }
+    }
+
+    public static void redirectActivity(Activity activity, Class secondActivity) {
+        Intent intent = new Intent(activity, secondActivity);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+        activity.finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
     }
 
     @Override
@@ -61,44 +127,44 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        if (item.getItemId() == R.id.nav_home)
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-
-        if (item.getItemId() == R.id.nav_incident_log)
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new IncidentLogFragment()).commit();
-
-        if (item.getItemId() == R.id.nav_wake_words)
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-
-        if (item.getItemId() == R.id.nav_communications)
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-
-        if (item.getItemId() == R.id.nav_emergency_contacts) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EmergencyContactsFragment()).commit();
-        }
-
-        if (item.getItemId() == R.id.nav_local_resources)
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
-
-        if (item.getItemId() == R.id.nav_profileSettings)
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileSettingsFragment()).commit();
-
-        if (item.getItemId() == R.id.nav_safe_exit)
-            safeExit();
-
-        if (item.getItemId() == R.id.nav_logout)
-        {
-            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
-            // Perform logout action
-            logout();
-        }
-
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return true;
-    }
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//
+//        if (item.getItemId() == R.id.nav_home)
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//
+//        if (item.getItemId() == R.id.nav_incident_log)
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new IncidentLogFragment()).commit();
+//
+//        if (item.getItemId() == R.id.nav_wake_words)
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//
+//        if (item.getItemId() == R.id.nav_communications)
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//
+//        if (item.getItemId() == R.id.nav_emergency_contacts) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EmergencyContactsFragment()).commit();
+//        }
+//
+//        if (item.getItemId() == R.id.nav_local_resources)
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+//
+//        if (item.getItemId() == R.id.nav_profileSettings)
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProfileSettingsFragment()).commit();
+//
+//        if (item.getItemId() == R.id.nav_safe_exit)
+//            safeExit();
+//
+//        if (item.getItemId() == R.id.nav_logout)
+//        {
+//            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+//            // Perform logout action
+//            logout();
+//        }
+//
+//        drawerLayout.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
 
     @Override
     public void onBackPressed() {
