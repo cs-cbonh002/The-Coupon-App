932627ed2538ff939deaee381d1e59c8f6ca4335
RedstoneRm <smcal004@odu.edu>
2024-03-24 21:06:45 -0400

    Referenced Edit Text fields and Button on ProfileSettingsFragment

--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/fragments/ProfileSettingsFragment.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/fragments/ProfileSettingsFragment.java
@@ -1,9 +1,12 @@
 package edu.odu.cs.teamblack.cs411.thecouponapp.ui.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +14,23 @@ import androidx.fragment.app.Fragment;
 
 import edu.odu.cs.teamblack.cs411.thecouponapp.R;
 
+
+
 public class ProfileSettingsFragment extends Fragment {
+
+    View view;
+    Button Familybutton;
+    Button Emergencybutton;
+    Button DangerAssebutton;
+    Button WakeWordbutton;
+    Button Themebutton;
+    Button Featurebutton;
+    Button Savebutton;
+    EditText FirstNamefield;
+    EditText LastNamefield;
+    EditText Emailfield;
+    EditText DOBfield;
+    EditText Genderfield;
 
     public ProfileSettingsFragment() {
     }
@@ -20,7 +39,24 @@ public class ProfileSettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_profilesettings, container, false);
+        view = inflater.inflate(R.layout.activity_profilesettings, container, false);
+        // Reference Edittext fields
+        FirstNamefield =  view.findViewById(R.id.editTextFirstName);
+        LastNamefield = view.findViewById(R.id.editTextLastName);
+        Emailfield = view.findViewById(R.id.editTextEmail);
+        DOBfield = view.findViewById(R.id.editTextDOB);
+        Genderfield = view.findViewById(R.id.editTextGender);
+        // Reference Buttons
+        Familybutton = view.findViewById(R.id.familyInfoButton);
+        Emergencybutton = view.findViewById(R.id.emergencyContactsButton);
+        DangerAssebutton = view.findViewById(R.id.dangerAssessmentButton);
+        WakeWordbutton = view.findViewById(R.id.wakeWordSettingsButton);
+        Themebutton = view.findViewById(R.id.themeSettingsButton);
+        Featurebutton = view.findViewById(R.id.featureSettingsButton);
+        Savebutton = view.findViewById(R.id.Savebutton);
+
+
+        return view;
     }
 
 }
