d18e2723cb2482aedf39a2aacccee8cb9c60505a
cs-cbonh002 <bonham_36@hotmail.com>
2024-03-30 16:46:39 -0400

    Start implementation of home fragment.

--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/fragments/HomeFragment.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/fragments/HomeFragment.java
@@ -4,9 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import edu.odu.cs.teamblack.cs411.thecouponapp.R;
 
 public class HomeFragment extends Fragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Button incidentLog, wakeWords, communications, emergencyContacts, localResources, profileAndSettings;
+
+
+    }
 }
