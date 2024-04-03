db3b91b6667e2e59741ea1e9584a5725138a05f8
cs-cbonh002 <bonham_36@hotmail.com>
2024-04-03 16:28:32 -0400

    Set up remaining fragments to their respective xml files.

--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/fragments/IncidentLogsFragment.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/fragments/IncidentLogsFragment.java
@@ -1,7 +1,22 @@
 package edu.odu.cs.teamblack.cs411.thecouponapp.ui.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import edu.odu.cs.teamblack.cs411.thecouponapp.R;
 
 public class IncidentLogsFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_incident_log, container, false);
+    }
 }
--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/fragments/LocalResourcesFragment.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/fragments/LocalResourcesFragment.java
@@ -1,8 +1,22 @@
 package edu.odu.cs.teamblack.cs411.thecouponapp.ui.fragments;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import edu.odu.cs.teamblack.cs411.thecouponapp.R;
 
 public class LocalResourcesFragment extends Fragment {
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.activity_local_resources, container, false);
+    }
 }
