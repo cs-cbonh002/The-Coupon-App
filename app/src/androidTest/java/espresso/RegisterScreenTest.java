aaba1d0f5f6b202e39d9110673b39f5a80b53e0e
cs-cbonh002 <bonham_36@hotmail.com>
2024-04-05 23:33:53 -0400

    Add additional test cases for Register Screen. Fix error on Register screen that was discovered during testing.

--- app/src/androidTest/java/espresso/RegisterScreenTest.java
+++ app/src/androidTest/java/espresso/RegisterScreenTest.java
@@ -1,6 +1,8 @@
 package espresso;
 
 import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.PositionAssertions.isCompletelyAbove;
 import static androidx.test.espresso.assertion.PositionAssertions.isCompletelyBelow;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
@@ -24,9 +26,55 @@ public class RegisterScreenTest {
             new ActivityScenarioRule<RegisterActivity>(RegisterActivity.class);
 
     @Test
-    public void registerButtonIsDisplayedBelowConfirmPasswordEditField()
+    public void welcomeBannerTest()
+    {
+        onView(withId(R.id.welcomeBanner)).check(matches(isDisplayed()));
+        onView(withId(R.id.welcomeBanner)).check(isCompletelyAbove(withId(R.id.userNameField)));
+        onView(withId(R.id.welcomeBanner)).check(matches(isNotClickable()));
+    }
+
+    @Test
+    public void userNameFieldTest()
+    {
+        onView(withId(R.id.userNameField)).check(matches(isDisplayed()));
+        onView(withId(R.id.userNameField)).check(isCompletelyBelow(withId(R.id.welcomeBanner)));
+        onView(withId(R.id.userNameField)).check(isCompletelyAbove(withId(R.id.passwordField)));
+        onView(withId(R.id.userNameField)).perform(typeText("typing test"));
+    }
+
+    @Test
+    public void passwordFieldTest()
+    {
+        onView(withId(R.id.passwordField)).check(matches(isDisplayed()));
+        onView(withId(R.id.passwordField)).check(isCompletelyBelow(withId(R.id.userNameField)));
+        onView(withId(R.id.passwordField)).check(isCompletelyAbove(withId(R.id.confirmPasswordField)));
+        onView(withId(R.id.passwordField)).perform(typeText("typing test"));
+    }
+
+    @Test
+    public void confirmPasswordFieldTest()
+    {
+        onView(withId(R.id.confirmPasswordField)).check(matches(isDisplayed()));
+        onView(withId(R.id.confirmPasswordField)).check(isCompletelyBelow(withId(R.id.passwordField)));
+        onView(withId(R.id.confirmPasswordField)).check(isCompletelyAbove(withId(R.id.newAccountMessage)));
+        onView(withId(R.id.confirmPasswordField)).perform(typeText("typing test"));
+    }
+
+    @Test
+    public void newAccountMessageTest()
+    {
+        onView(withId(R.id.newAccountMessage)).check(matches(isDisplayed()));
+        onView(withId(R.id.newAccountMessage)).check(isCompletelyBelow(withId(R.id.confirmPasswordField)));
+        onView(withId(R.id.passwordField)).check(isCompletelyAbove(withId(R.id.login_button)));
+        onView(withId(R.id.newAccountMessage)).check(matches(isNotClickable()));
+    }
+
+    @Test
+    public void registerButtonTest()
     {
         onView(withId(R.id.register_button)).check(matches(isDisplayed()));
         onView(withId(R.id.register_button)).check(isCompletelyBelow(withId(R.id.confirmPasswordField)));
+        onView(withId(R.id.register_button)).check(isCompletelyAbove(withId(R.id.newAccountMessage)));
+        onView(withId(R.id.register_button)).check(matches(isClickable()));
     }
 }
