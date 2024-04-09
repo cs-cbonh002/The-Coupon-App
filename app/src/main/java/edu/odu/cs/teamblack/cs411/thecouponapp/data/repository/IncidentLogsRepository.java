8db5c6edd5500231e65f4457c355e720dc59143a
James MacDonald <jmacd007@odu.edu>
2024-04-09 16:05:28 -0400

    setup incident logs framework

---
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/data/repository/IncidentLogsRepository.java
@@ -0,0 +1,4 @@
+package edu.odu.cs.teamblack.cs411.thecouponapp.data.repository;
+
+public class IncidentLogsRepository {
+}
---
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/viewmodels/IncidentLogsViewModel.java
@@ -0,0 +1,4 @@
+package edu.odu.cs.teamblack.cs411.thecouponapp.ui.viewmodels;
+
+public class IncidentLogsViewModel {
+}
