a4ffc94fb1d4e80f0f92d80de036917a03a4adc0
cs-cbonh002 <bonham_36@hotmail.com>
2024-04-14 16:09:48 -0400

    Remove settings button from Login screen.

--- app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/activities/LoginActivity.java
+++ app/src/main/java/edu/odu/cs/teamblack/cs411/thecouponapp/ui/activities/LoginActivity.java
@@ -48,13 +48,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button settingsButton = findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(v -> {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            intent.putExtra("fragmentToLoad", "SettingsFragment"); // Indicate which fragment to load
-            startActivity(intent);
-            finish();
-        });
+
 
         Button homeButton = findViewById(R.id.homeButton);
         homeButton.setOnClickListener(v -> {
